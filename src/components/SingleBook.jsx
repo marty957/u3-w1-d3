7e00bd5b24f22cb1b8ce7ff1945c0a91f5fdc0fc
marty957 <fantuzzimartina@yahoo.it>
2024-11-27 16:44:38 +0100

    second commit

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -7,14 +7,19 @@ class SingleBook extends Component {
   state = {
     selected: false
   };
+  handleChange = () => {
+    this.setState({ selected: !this.state.selected });
+    console.log(this.state);
+  };
 
   render() {
+    const newClass = this.state.selected ? "border border-danger" : "";
     return (
-      <Col xs={6} md={4} xl={3} key={this.propsprop.asin}>
-        <Card>
-          <Card.Img variant="top" src={this.prop.src} height={"150px"} alt="copertina" />
+      <Col xs={6} md={4} xl={3} key={this.props.asin}>
+        <Card onClick={this.handleChange} className={newClass}>
+          <Card.Img variant="top" src={this.props.src} height={"150px"} alt="copertina" />
           <Card.Body>
-            <Card.Title className="text-truncate">{this.prop.title}</Card.Title>
+            <Card.Title className="text-truncate">{this.props.title}</Card.Title>
           </Card.Body>
         </Card>
       </Col>
